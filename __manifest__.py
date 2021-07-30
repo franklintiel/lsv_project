a981b3660a02331b745785d00138e2f5d148c731
Franklin Sarmiento <franklinitiel@franklinitiel.franklinitiel.local>
2021-07-30 13:06:32 -0500

    Initial files have been added

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "LSV Common features (Project)",
+    'version': "1.0-dev1",
+    'summary': "",
+    'depends': ['project', 'contacts'],
+    'data': {},
+    'installable': True,
+    'auto_install': False,
+    'license': "LGPL-3",
+}
